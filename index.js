89658659f29602cc182e6acbb89529ee746e452b
balaraju404 <balarajugandham1@gmail.com>
2024-05-21 11:19:04 +0530

    update api-access

--- index.js
+++ index.js
@@ -14,14 +14,6 @@ app.use(express.json());
 // Set up your routes
 app.use(router);
 
-// Custom CORS headers middleware
-app.use((req, res, next) => {
-    // res.setHeader('Access-Control-Allow-Origin', process.env.FRONTEND_URL || 'http://localhost:4200');
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, PATCH, DELETE');
-    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-    next();
-});
-
 app.listen(3001, () => {
-    console.log("Server is running on port 3000");
+    console.log("Server is running on port 3001");
 });
