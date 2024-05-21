b5f57338849b70f7e17b6f8f19f40dd5a40de28e
balaraju404 <balarajugandham1@gmail.com>
2024-05-21 11:09:59 +0530

    update api-access

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ app.use(router);
 
 // Custom CORS headers middleware
 app.use((req, res, next) => {
-    res.setHeader('Access-Control-Allow-Origin', process.env.FRONTEND_URL || 'http://localhost:4200');
+    // res.setHeader('Access-Control-Allow-Origin', process.env.FRONTEND_URL || 'http://localhost:4200');
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, PATCH, DELETE');
     res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
     next();
