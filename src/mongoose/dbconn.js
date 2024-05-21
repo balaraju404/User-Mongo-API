8287137e5b940f9f893f909de0ae54cc64cc0693
balaraju404 <balarajugandham1@gmail.com>
2024-05-21 10:42:33 +0530

    update mongo-conn

--- src/mongoose/dbconn.js
+++ src/mongoose/dbconn.js
@@ -3,11 +3,10 @@ import mongoose from "mongoose";
 // const mongo_uri = process.env.MONGO_URL || "mongodb+srv://gandhambalaraju18:Balaraju%4018@cluster0.zresrux.mongodb.net/balaraju";
 const mongo_uri = process.env.MONGO_URL || "mongodb://localhost:27017/balaraju";
 
-mongoose.connect(mongo_uri, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true
-}).then(() => {
-    console.log("DB Connected!");
-}).catch((error) => {
-    console.log("DB Connection Failed! and error is : ", error);
-});
+mongoose.connect(process.env.MONGO_URL || 'mongodb+srv://gandhambalaraju18:Balaraju%4018@cluster0.zresrux.mongodb.net/task-manager')
+    .then(() => {
+        console.log("DB connected")
+    }).catch((err) => {
+        console.error("Error connecting to database:", err.message);
+    });
+
